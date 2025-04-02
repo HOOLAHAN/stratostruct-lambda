1b55517362cc6f01895ed98555446f57274359a5
Iain <iain.hoolahan@gmail.com>
2025-04-02 19:34:05 +0100

    added CORS to handler and indentation to yaml

--- handler.js
+++ handler.js
@@ -1,4 +1,19 @@
 const serverless = require('serverless-http');
 const app = require('./server');
 
+// Middleware to handle CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    res.status(200).send(); // Handle preflight request
+    return;
+  }
+
+  next();
+});
+
 module.exports.handler = serverless(app);
